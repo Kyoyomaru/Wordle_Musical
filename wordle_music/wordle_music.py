87ffac8c7be404e625b59e10303dccbbc491f03e
Kyoyomaru <miguelzuluagabalmaceda@gmail.com>
2025-11-05 23:31:51 -0500

    1 commit WordleMusical

--- wordle_music/wordle_music.py
+++ wordle_music/wordle_music.py
@@ -3,3 +3,19 @@ import random
 import unicodedata
 from pydub import AudioSegment
 from io import BytesIO
+
+
+class WordleMusical:
+    def __init__(self,jugador: Jugador,canciones:list):
+        self.jugador = jugador
+        self.canciones = canciones
+        self.cancion_actual = None
+        self.tablero = []
+
+    def seleccionar_cancion(self):
+        self.cancion_actual = random.choice(self.canciones)
+
+
+
+
+
