aefbeca0b1a055a2b8a3adbda217671a50630f4a
Kyoyomaru <miguelzuluagabalmaceda@gmail.com>
2025-11-05 23:35:08 -0500

    2 commit WordleMusical

--- wordle_music/wordle_music.py
+++ wordle_music/wordle_music.py
@@ -15,7 +15,21 @@ class WordleMusical:
     def seleccionar_cancion(self):
         self.cancion_actual = random.choice(self.canciones)
 
+    def inicializar_tablero(self):
+        self.tablero = []
+        self.jugador.reiniciar_intentos()
+        return len(self.cancion_actual.titulo_original)
+    def validar_intento(self,palabra:str):
+        self.jugador.registrar_letras(palabra)
+        resultado=self.cancion_actual.comparar_letras(palabra)
+        self.tablero.append(resultado)
 
+        palabra_normalizada = normalizar_texto(palabra)
+        if palabra_normalizada == self.cancion_actual.titulo_original:
+            return True
+        else:
+                self.jugador.restar_intento()
+                return False
 
 
 
